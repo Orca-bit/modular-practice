c579185feea02a6d8ad810d5b41c91d31f32afe2
steepcurve <steepcurve@163.com>
2025-05-24 17:01:09 +0800

    feat: 添加自定义注意力机制操作及测试
    添加了自定义的注意力机制操作 `fused_attention_custom`，包括 CPU 和 GPU 的实现，并提供了相应的测试文件。该操作基于 FlashAttention-2 论文中的算法，支持分块计算以提高效率。

---
+++ custom_op/test_attentions.py
@@ -0,0 +1,33 @@
+import torch
+
+from pathlib import Path
+from max.torch import CustomOpLibrary
+
+mojo_kernels = Path(__file__).parent / "operations"
+op_library = CustomOpLibrary(mojo_kernels)
+mojo_fused_attention = op_library.fused_attention_custom[
+    {
+        "BN": 8,
+        "BD": 8,
+    }
+]
+
+
+def fused_attention(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor) -> torch.Tensor:
+    m, _ = q.shape
+    _, n = v.shape
+    result = torch.zeros((m, n), dtype=q.dtype, device=q.device)
+    mojo_fused_attention(result, q, k, v)
+    return result
+
+
+def main():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"device: {device}")
+    q = torch.randn((40, 128), dtype=torch.float32, device=device)
+    k = torch.randn((40, 128), dtype=torch.float32, device=device)
+    v = torch.randn((40, 128), dtype=torch.float32, device=device)
+    print(fused_attention(q, k, v))
+    
+if __name__ == "__main__":
+    main()
